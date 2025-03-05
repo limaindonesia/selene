821c23c9f81c8a3099f891ca0ff2b7339e750b84
Putri Alvita <alvita@perqara.com>
2025-03-05 09:15:02 +0700

    ZRF-35 feat: refactor price formatting

--- src/models/LegalForm.ts
+++ src/models/LegalForm.ts
@@ -1,13 +1,15 @@
 import mongoose, { Schema, Document, Connection, Model } from "mongoose";
+import { formatToRupiah } from "../utils/utils";
 
 export interface ILegalForm extends Document {
   id: string;
   name: string;
+  slug: string;
   category: string;
   description: string;
   status: string;
-  price: string;
-  final_price: string;
+  price: number;
+  final_price: number;
   keywords: string[];
   picture_url: string;
   template_doc_id: string;
@@ -15,6 +17,8 @@ export interface ILegalForm extends Document {
   form_detail: FormDetail[];
   rating: string;
   total_created: number;
+  formatted_price: string;
+  formatted_final_price: string;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -48,8 +52,8 @@ const LegalFormSchema = new Schema<ILegalForm>({
   category: { type: String, ref: "m_categories", required: true },
   description: { type: String, required: true },
   status: { type: String, required: true },
-  price: { type: String, required: false },
-  final_price: { type: String, required: true },
+  price: { type: Number, required: false },
+  final_price: { type: Number, required: true },
   keywords: { type: [String], required: false },
   picture_url: { type: String, required: true },
   template_doc_id: { type: String, required: true },
@@ -61,7 +65,17 @@ LegalFormSchema.virtual("id").get(function () {
   return this._id.toString();
 });
 
+LegalFormSchema.virtual("formatted_price").get(function () {
+  return formatToRupiah(this.price);
+});
+
+LegalFormSchema.virtual("formatted_final_price").get(function () {
+  return formatToRupiah(this.final_price);
+});
+
 LegalFormSchema.set("toJSON", { virtuals: true });
+LegalFormSchema.set("toObject", { virtuals: true });
+
 
 export const LegalFormModel = (connection: Connection): Model<ILegalForm> => {
   return connection.model<ILegalForm>("m_legal_forms", LegalFormSchema);
